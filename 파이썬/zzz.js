12fcc9af60c3532d623763a49b82f1bbdde98e72
csjun9900 <kjsug1030@naver.com>
2022-06-13 23:06:38 +0900

    자바스크립트 문법 복습

--- 파이썬/zzz.js
+++ 파이썬/zzz.js
@@ -1,6 +1,17 @@
-const user = {
-  name: "Sejun",
-  age: 24,
-};
+let user = [1, 2, 3, 4, 5, 6, 7, 8];
 
-console.log(user);
+let userList = [
+  { name: "mike", age: 30 },
+  { name: "kay", age: 19 },
+  { name: "brory", age: 22 },
+];
+
+let result = userList.map((user, index) =>
+  Object.assign({}, user, { id: index + 1, isAdult: user.age > 19 })
+);
+
+let arr = ["브로리,카카로드,배지터"];
+
+let result2 = arr.split(",");
+
+console.log(result2);
