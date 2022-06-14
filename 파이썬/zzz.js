6df7592ad5ef1c58e95235ccb8000b517e4c377a
csjun9900 <kjsug1030@naver.com>
2022-06-14 22:07:17 +0900

    js 문법 연습

--- 파이썬/zzz.js
+++ 파이썬/zzz.js
@@ -6,12 +6,49 @@ let userList = [
   { name: "brory", age: 22 },
 ];
 
-let result = userList.map((user, index) =>
-  Object.assign({}, user, { id: index + 1, isAdult: user.age > 19 })
-);
+let userName = ["mike", "kay", "brory"];
 
-let arr = ["브로리,카카로드,배지터"];
+let arr = [27, 8, 5, 13];
 
-let result2 = arr.split(",");
+arr.sort((a, b) => {
+  return a - b;
+});
 
-console.log(result2);
+let result = userList.reduce((acc, cur) => {
+  if (cur.age > 19) {
+    acc.push(cur.name);
+  }
+  return acc;
+}, []);
+
+let [user1, user2, user3] = userName;
+
+function showName(...num) {
+  let result = num.reduce((n, cur) => n + cur);
+  // console.log(result);
+}
+
+function User(name, age, ...skils) {
+  this.name = name;
+  this.age = age;
+  this.skils = skils;
+}
+
+const sana = new User("사나", 26, "코딩", "일본어", "한국어");
+const 카카로트 = new User("손오공", 999, "초사이언", "에네르기파");
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arrResult = [...arr2, ...arr1];
+
+function makeCounter() {
+  let num = 0;
+
+  return function () {
+    return num++;
+  };
+}
+
+let counter = makeCounter();
+console.log(counter());
+console.log(counter());
